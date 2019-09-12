6bda479381e24d4a747bbf88e9c15a269ef6ec99
alex <cyy0523xc@gmail.com>
2019-09-12 15:50:08 +0800

    port

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ echo "Command: $cmd"
 
 docker rm -f ibbd-yolov3
 docker run --rm "$args" --runtime=nvidia --name ibbd-yolov3 \
-    -p 20960:20960 \
+    -p 20970:20960 \
     -v `pwd`:/yolov3 \
     -e PYTHONIOENCODING=utf-8 \
     -w /yolov3 \
